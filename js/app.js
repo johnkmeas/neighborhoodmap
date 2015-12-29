fcc4a7f658b62d5819a3620f9b701ae0c6e12b62
John Meas <johnkmeas@gmail.com>
2015-12-29 01:17:52 -0800

    final touch up before submission

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // I've declared the global variables above everything
 var map, markerd,
-
   viewModel = {
     park : [
       {name: "Tynehead Regional Park", place: {lat: 49.181365, lng: -122.757452},
@@ -31,7 +30,7 @@ var map, markerd,
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   // Call markers function to load the map markers
   markers();
-};
+}
 // Load the Map
 google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -54,14 +53,14 @@ var markers = function() {
     for(var i = 0; i < 6; i++){
       var place = parks[i].place;
       var drop = google.maps.Animation.DROP;
-      var name = parks[i].name; 
+      var name = parks[i].name;
       marker = new google.maps.Marker({
         position: place,
         map: map,
         animation: drop,
         title: name
       });
-      // Assign each marker to as markerd array for 
+      // Assign each marker to as markerd array for
       // later access.
       markerd[i] = marker;
 
@@ -88,9 +87,7 @@ var ViewModel = function(records) {
   // Turns markerd to an observable array
   markerd = ko.observableArray([]);
 
-  // 
   var self = this;
-    
   // records is an observable array that creates a new Record object
   self.records = ko.observableArray(
   ko.utils.arrayMap(records, function(i) {
@@ -99,7 +96,6 @@ var ViewModel = function(records) {
 
   // nameSearch aka filter is observable
   self.nameSearch = ko.observable('');
- 
   // Computes the contents of foreach: parks in the html
   // based on what is observed within nameSearch,
   // it will call the ko.utils.stringStartsWith function
@@ -107,87 +103,83 @@ var ViewModel = function(records) {
   // both in lowerCase.
   self.parks = ko.computed(function() {
     return ko.utils.arrayFilter(self.records(), function(r) {
-    return (self.nameSearch().length == 0 || ko.utils.stringStartsWith(r.name.toLowerCase(), self.nameSearch().toLowerCase()))      
+    return (self.nameSearch().length == 0 || ko.utils.stringStartsWith(r.name.toLowerCase(), self.nameSearch().toLowerCase()))
     });
   });
 
   // Click function that triggers the map marker
   // from the list of names.
-
-
-
   clicker = function(){
     for(var i = 0; i < 6; i++){
       if(this.name === parks[i].name){
         google.maps.event.trigger(markerd[i], 'click');
+        // Triggers the toggle to close navbar list
+        // in the mobile dimensions
         $( ".navbar-toggle" ).trigger( "click" );
-        
       }
-    }        
+    }
   };
 
-  // Flickr api 
-  var flickrkey = '29e62f8cfc70c951fad22716511f1002',
-    flickrUrl = 'https://api.flickr.com/services',
-    flickrSecret = 'cee13e67a6a7fe71';
 
-      $.ajax({
-    url: flickrUrl,
-    dataType: "jsonp",
-    // jsonp: "callback",
-    success: function( response ){
-      console.log(response)
-    
-    }
-  });
 };
 
+//variable for wikipedia api
 var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
 var wikiCall = '&format=json&callback=wikiCallback';
 var wikiUrlist = [];
 var wikiSpot = [
-  'tynehead', 'green_timbers', 'capilanoriver', 'Burnaby Fraser Foreshore', 'Campbell Valley Regional Park'
+  'surrey_bc', 'green_timbers_lake', 'capilano_river', 'Burnaby', 'fort_langley'
 ];
 var wikiarticleArray = [];
+var wikiRequestTimeout = setTimeout(function(){
+  $wikiElem.text("failed to get wikipedia resources");
+}, 8000);
 
-for(var i = 0; i < 6; i++){
-  
-  wikiSpot[i]
+for(var i = 0; i < 5; i++){
+  wikiUrlist.push(wikiUrl + wikiSpot[i] + wikiCall);
+  console.log(wikiUrlist[i]);
+};
 
-  wikiUrlist.push(wikiUrl + wikiSpot[i] + wikiCall)
-  console.log(wikiUrlist[i])
-}
+
+
   // Variables for the wikipedia api
-  var $wikiElem = $('#wikipedia-links');
-  $wikiElem.text("");
-  //var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + 'Vancouver' +'&format=json&callback=wikiCallback';
+var $wikiElem = $('#wikipedia-links');
+$wikiElem.text("");
+var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + wikiSpot[2] +'&format=json&callback=wikiCallback';
+var wurl = [];
+var articleList;
+for(var i = 0; i < 5; i++){
+  $.ajax({
+    url: wikiUrlist[i],
+    dataType: "jsonp",
+    jsonp: "callback",
+    success: function( response ){
+      articleList = response[1];
+      console.log(response)
+      for (var i = 0; i < articleList.length; i++){
+        articleStr = articleList[i];
+        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+        $wikiElem.append('<li><a href="'+ url +'">' + articleStr + '</a></li>');
+      };
+       // My intention is to
+       // push an article from wikipedia into
+       // an array that can be passed into an
+       // infowindow that corresponds with the article.
 
-var wikiInit = function(){
-  
-    $.ajax({
-      url: wikiUrlist[1],
-      dataType: "jsonp",
-       jsonp: "callback",
-      success: function( response ){
-        console.log(response)
-        var articleList = response[1];
-        
-        for (var i = 0; i < 4; i++){
-          articleStr = articleList[i];
-          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-          $wikiElem.append('<li><a href="'+ url +'">' + articleStr + '</a></li>');
-        };
-  
-        clearTimeout(wikiRequestTimeout);
-      }
-    });
-  
+       // Could use a push in the right direction:)
 
+
+        wikiarticleArray.push(wurl);
+
+        console.log(wikiarticleArray);
+
+
+      clearTimeout(wikiRequestTimeout);
+    }
+  });
 }
 
-wikiInit();
-
-
+// this is need for the toggling of the sidebar nav in mobile
 $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
--- js/search.js
+++ js/search.js
@@ -11,9 +11,24 @@
 };
 // Load the Map
 google.maps.event.addDomListener(window, 'load', initialize);
+  // Flickr api 
+var apiKey = '0b8f5be71394506b9809998b0caaec59';
+var secret = '575cfa465225bbf5';
+http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=29e62f8cfc70c951fad22716511f1002&user_id=user_id_x&format=json
+ 
+var ul = $('#gallery');
+var url = 'http://api.flickr.com/services/feeds/photos_public.gne?id=45038025@N00&format=json&jsoncallback=?';
+imgs = '';
 
-$(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
-});
+$.getJSON(url, function(data) {
+    imgs = data;
+
+    $(data.items).each(function(i, item) {
+        ul.append($('<li><img src="' + item.media.m.slice(0,-5) + 'b.jpg" /><div class="caption">'+item.title+'</div></li>'))
+        if (i == 40) return false
+    });
+    $('#images div').click(function() {
+        $('#images div').removeClass('selected')
+        $(this).addClass('selected')
+    });
+})
